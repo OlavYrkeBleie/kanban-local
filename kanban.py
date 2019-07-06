eccb2fb326e8ed7e2ab1ab9369bb6fdbad6044f8
OlavYrkeBleie <224439981+OlavYrkeBleie@users.noreply.github.com>
2019-07-06 20:05:00 +0200

    save board to json, load on startup

--- kanban.py
+++ kanban.py
@@ -3,27 +3,45 @@
 
 import tkinter as tk
 from tkinter import messagebox, simpledialog
-import re
+import json, os, re
 
-COLS   = ["To Do", "In Progress", "Done"]
-APP_BG = "#f4f4f4"
-COL_BG = "#e2e8ee"
-CARD_W = 230
+COLS    = ["To Do", "In Progress", "Done"]
+APP_BG  = "#f4f4f4"
+COL_BG  = "#e2e8ee"
+CARD_W  = 230
+SAVE_PATH = os.path.join(os.path.expanduser("~"), "kanban_board.json")
 
 class Card:
     def __init__(self, title, body=""):
         self.title = title
         self.body  = body
         self.tags  = re.findall(r'@(\w+)', body)
+    def to_dict(self): return {"title":self.title,"body":self.body}
+    @classmethod
+    def from_dict(cls,d): return cls(d["title"],d.get("body",""))
 
-cards = {c: [] for c in COLS}
+board = {c: [] for c in COLS}
 col_frames = {}
+
+def save():
+    with open(SAVE_PATH,"w") as f:
+        json.dump({c:[card.to_dict() for card in board[c]] for c in COLS},f,indent=2)
+
+def load():
+    if os.path.exists(SAVE_PATH):
+        try:
+            with open(SAVE_PATH) as f:
+                data = json.load(f)
+            for c in COLS:
+                board[c] = [Card.from_dict(d) for d in data.get(c,[])]
+        except Exception:
+            pass
 
 def refresh():
     for col, frame in col_frames.items():
         for w in frame.winfo_children():
             w.destroy()
-        for card in cards[col]:
+        for card in board[col]:
             build_card(frame, card, col)
 
 def build_card(parent, card, col):
@@ -39,34 +57,31 @@ def build_card(parent, card, col):
     ci = COLS.index(col)
     if ci > 0:
         tk.Button(bf, text="←", bd=0, bg="#e8e8e8",
-                  command=lambda c=card,fc=col,tc=COLS[ci-1]: move(c,fc,tc)).pack(side="left", padx=1)
+                  command=lambda c=card,fc=col,tc=COLS[ci-1]: move(c,fc,tc)).pack(side="left",padx=1)
     if ci < len(COLS)-1:
         tk.Button(bf, text="→", bd=0, bg="#e8e8e8",
-                  command=lambda c=card,fc=col,tc=COLS[ci+1]: move(c,fc,tc)).pack(side="left", padx=1)
+                  command=lambda c=card,fc=col,tc=COLS[ci+1]: move(c,fc,tc)).pack(side="left",padx=1)
     tk.Button(bf, text="✕", bd=0, bg="#ffd0d0",
-              command=lambda c=card,co=col: delete(c,co)).pack(side="right", padx=1)
+              command=lambda c=card,co=col: delete(c,co)).pack(side="right",padx=1)
 
-def move(card, from_col, to_col):
-    cards[from_col].remove(card)
-    cards[to_col].append(card)
-    refresh()
+def move(card,from_col,to_col):
+    board[from_col].remove(card); board[to_col].append(card); save(); refresh()
 
-def delete(card, col):
-    if messagebox.askyesno("Delete", f'Delete "{card.title}"?'):
-        cards[col].remove(card)
-        refresh()
+def delete(card,col):
+    if messagebox.askyesno("Delete",f'Delete "{card.title}"?'):
+        board[col].remove(card); save(); refresh()
 
 def add_card(col):
-    title = simpledialog.askstring("New card", "Title:", parent=root)
+    title = simpledialog.askstring("New card","Title:",parent=root)
     if title and title.strip():
-        body = simpledialog.askstring("New card", "Notes (@tag to mention):", parent=root) or ""
-        cards[col].append(Card(title.strip(), body))
-        refresh()
+        body = simpledialog.askstring("New card","Notes (@tag to mention):",parent=root) or ""
+        board[col].append(Card(title.strip(),body)); save(); refresh()
 
 root = tk.Tk()
 root.title("kanban-local")
 root.geometry("900x580")
 root.configure(bg=APP_BG)
+load()
 
 for col in COLS:
     outer = tk.Frame(root, bg=COL_BG, width=270)
@@ -74,12 +89,12 @@ for col in COLS:
     outer.pack_propagate(False)
     hdr = tk.Frame(outer, bg=COL_BG)
     hdr.pack(fill="x", padx=6, pady=(8,4))
-    tk.Label(hdr, text=col, bg=COL_BG, font=("Segoe UI", 9, "bold")).pack(side="left")
-    tk.Button(hdr, text="+", bd=0, bg=COL_BG,
-              font=("Segoe UI", 13, "bold"), fg="#27ae60",
+    tk.Label(hdr, text=col, bg=COL_BG, font=("Segoe UI",9,"bold")).pack(side="left")
+    tk.Button(hdr, text="+", bd=0, bg=COL_BG, font=("Segoe UI",13,"bold"), fg="#27ae60",
               command=lambda c=col: add_card(c)).pack(side="right")
     inner = tk.Frame(outer, bg=COL_BG)
     inner.pack(fill="both", expand=True)
     col_frames[col] = inner
 
+refresh()
 root.mainloop()
